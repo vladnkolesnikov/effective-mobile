edf60857ee92bf57a48b8323b64f883baad75a76
Vladislav Kolesnikov <forshortmrmeth@gmail.com>
2025-07-23 16:54:48 +0200

    update formatting

--- migrations/00002_user_subscriptions.sql
+++ migrations/00002_user_subscriptions.sql
@@ -2,21 +2,18 @@
 -- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS user_subscriptions
 (
-    id         UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    name       VARCHAR(50)   NOT NULL,
-    price      DECIMAL(5, 0) NOT NULL,
-    
-    start_date DATE NOT NULL,
+    id              UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name            VARCHAR(50)   NOT NULL,
+    price           DECIMAL(5, 0) NOT NULL,
+    start_date      DATE          NOT NULL,
     expiration_date DATE,
-    
-    user_id    UUID REFERENCES users(id) ON DELETE CASCADE,
-    
-    created_at timestamptz      DEFAULT CURRENT_TIMESTAMP,
-    updated_at timestamptz      DEFAULT CURRENT_TIMESTAMP,
+    user_id         UUID REFERENCES users (id) ON DELETE CASCADE,
+    created_at      timestamptz      DEFAULT CURRENT_TIMESTAMP,
+    updated_at      timestamptz      DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT valid_exp_date CHECK (
         start_date < expiration_date
-    )
+        )
 );
 -- +goose StatementEnd
 
